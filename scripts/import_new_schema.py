d1e1de732c06dd2910e0712fdeefa6c6839d7d00
thm <thm.ar@outlook.com>
2024-02-04 14:46:00 +0000

    add new raw data sources, update import script enable overwrite files in import script add missing proof urls to existing imported data

--- scripts/import_new_schema.py
+++ scripts/import_new_schema.py
@@ -5,6 +5,7 @@ import os
 from glob import glob
 from unidecode import unidecode
 import yaml
+import pdb
 
 # brand/company fields
 NAME = "name"
@@ -35,10 +36,20 @@ existing_files = glob(brands_path + "*.yaml") + glob(companies_path + "*.yaml")
 
 owner_regex = re.compile(r"[O|o]wned [B|b]y \*\*(.+?)(?:\.+)?\*\*")
 
+witness_owner_regex = re.compile(r"[B|b]y ([\S]+)")
+
 
 def parent_from_details(details):
     if details:
         match = owner_regex.search(details)
+        if match:
+            return match.group(1)
+    return ""
+
+
+def witness_parent_from_details(details):
+    if details:
+        match = witness_owner_regex.search(details)
         if match:
             return match.group(1)
     return ""
@@ -106,7 +117,10 @@ def create_data_models(data):
     brands_yaml_data, companies_yaml_data = {}, {}
     for row in data:
         brand_name = row.get("attributes").get("name")
-        proof = row.get("attributes").get("proof")
+        proof_raw = row.get("attributes").get("proof")
+        proof_url = row.get("attributes").get("proofUrl")
+        proof = f"{proof_raw}[^1]\n\n[^1]: {proof_url}"
+
         parent_name = parent_from_details(proof).lower()
         image_url = row.get("attributes").get("imageUrl")
         reason = (
@@ -151,10 +165,53 @@ def create_data_models(data):
     return brands_yaml_data, companies_yaml_data
 
 
+def create_data_models_from_witness_json(data):
+    brands_yaml_data, companies_yaml_data = {}, {}
+
+    for row in data:
+        brand_name = row.get("name")
+        desc = row.get("reason")
+        how_to_boycott = "\n".join(row.get("howToBoycott"))
+        proof_url = row.get("source")
+        proof = f"{desc}[^1]\n\n{how_to_boycott}\n[^1]: {proof_url}"
+        categories = row.get("category")
+        parent_name = witness_parent_from_details(row.get("description")).lower()
+        image_url = row.get("logo")
+        reason = (
+            "operations_in_settlements"
+            if "settlement" in proof.lower()
+            else "operations_in_israel"
+        )
+
+        alternatives_txt = "\n".join(row.get("alternatives"))
+
+        if not brand_name:
+            continue
+
+        brands_yaml_data[brand_name] = {
+            NAME: brand_name,
+            STATUS: "avoid",
+            DESCRIPTION: proof,
+            REASONS: [reason],
+            COUNTRIES: ["global"],
+            CATEGORIES: categories,
+        }
+        if image_url:
+            brands_yaml_data[brand_name][LOGO_URL] = image_url
+        if alternatives_txt:
+            brands_yaml_data[brand_name][ALTERNATIVES_TEXT] = alternatives_txt
+        if parent_name:
+            brands_yaml_data[brand_name][STAKEHOLDERS] = [
+                {ID: name_to_id(parent_name), TYPE: "owner"}
+            ]
+
+    return brands_yaml_data, companies_yaml_data
+
+
 # Write yaml data to the given path, if there is no file already there.
 # Returns True if a file was written, false otherwise.
-def write_yaml_if_not_exists(file_path, data):
-    if file_path not in existing_files:
+def write_yaml_if_not_exists(file_path, data, overwrite):
+    if file_path not in existing_files or overwrite:
         with open(file_path, "w", encoding="utf-8") as yaml_file:
             print(f"writing: {file_path}")
             yaml.dump(data, yaml_file, default_flow_style=False, sort_keys=False)
@@ -164,28 +221,33 @@ def write_yaml_if_not_exists(file_path, data):
 
 
 def get_filename(name):
-
     return "".join(x for x in unidecode(name.lower()) if x.isalnum()) + ".yaml"
 
 
-def import_data(file_name):
+def import_data(file_name, input_type, overwrite):
     with open(file_name, encoding="utf-8") as fh:
         raw_data = json.load(fh)
+        brands = {}
+        companies = {}
 
-        boycott_brands, companies = create_data_models(raw_data)
-        alt_brands = create_alternatives_data_models(raw_data)
-        brands = boycott_brands | alt_brands
+        if input_type == "bds":
+            boycott_brands, companies = create_data_models(raw_data)
+            alt_brands = create_alternatives_data_models(raw_data)
+            brands = boycott_brands | alt_brands
+
+        if input_type == "witness":
+            brands, companies = create_data_models_from_witness_json(raw_data)
 
         files_written = 0
 
         for brand, details in brands.items():
             file_path = os.path.join(brands_path, get_filename(brand))
-            if write_yaml_if_not_exists(file_path, details):
+            if write_yaml_if_not_exists(file_path, details, overwrite):
                 files_written += 1
 
         for company, details in companies.items():
             file_path = os.path.join(companies_path, get_filename(company))
-            if write_yaml_if_not_exists(file_path, details):
+            if write_yaml_if_not_exists(file_path, details, overwrite):
                 files_written += 1
 
         file_or_files = "files" if files_written > 1 else "file"
@@ -195,10 +257,14 @@ def import_data(file_name):
 if __name__ == "__main__":
     args = sys.argv[1:]
 
-    if len(args) != 1:
-        print("Usage: python3 import_new_schema.py <path to JSON file to import>")
+    if len(args) < 2 or args[1] not in ("witness", "bds", "disoccupied"):
+        print(
+            "Usage: python3 import_new_schema.py <path to JSON file to import> <type witness|disoccupied|bds> <overwrite true, defaults to false>"
+        )
         sys.exit(1)
 
     input_file = args[0]
+    input_type = args[1]
+    overwrite = len(args) > 2
 
-    import_data(input_file)
+    import_data(input_file, input_type, overwrite)
